4e572a86c06a4a8c438597d2ae4b9ef6871570ae
Dawid <dawidpawlak268@gmail.com>
2022-05-18 21:54:55 +0200

    fix

--- config.index.ts
+++ config.index.ts
@@ -2,11 +2,14 @@ import { writeFile } from "fs";
 
 const targetPath = "./src/environments/environment.prod.ts";
 
+console.log(targetPath);
+
 const envConfigFile = `export const environment = {
    production: true,
    testEnv: '${process.env.TEST}'
  };`
 ;
+console.log(envConfigFile);
 
 writeFile(targetPath, envConfigFile, "utf8", (err) => {
   if (err) {
